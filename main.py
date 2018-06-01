ad49a555d33194d362f01add62203a96e33fd199
blitzingeagle <mcmorrischen@gmail.com>
2018-06-01 05:13:36 +0000

    open frame.txt

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ if len(glob("yolo.weights")) == 0:
     os.system("wget https://pjreddie.com/media/files/yolo.weights")
 
 
-def process_video(video):
-    output_dir = os.path.splitext(video)[0].replace("input", "output")
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+def process_video(video, output_dir):
+    cmd = "darknety detector demo cfg/coco.data cfg/yolo.cfg yolo.weights {} -prefix {}".format(video, os.path.join(output_dir, "frame"))
+    print(">", cmd)
+    os.system(cmd)
 
-    os.system("darknet detector demo cfg/coco.data cfg/yolo.cfg yolo.weights {} -prefix {}".format(video, os.path.join(output_dir, "frame")))
+
+def crop_images(output_dir, tag="car"):
+    target_dir = os.path.join(output_dir, tag)
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    with open(os.path.join(output_dir, "frame.txt")) as f:
+        content = f.readlines()
 
 
 input_dirs = glob("input/*")
@@ -21,7 +28,12 @@ for input_dir in input_dirs:
     videos = glob(os.path.join(input_dir, "*.avi"))
 
     for video in videos:
-        process_video(video)
+        output_dir = os.path.splitext(video)[0].replace("input", "output")
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        process_video(video, output_dir)
+        crop_images(output_dir)
 
 
 
