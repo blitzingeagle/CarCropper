ede50c6fc2d6577539c35dfc8579336a0d166d91
blitzingeagle <mcmorrischen@gmail.com>
2018-06-01 03:16:09 +0000

    Ignore weights

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import os
-os.environ["PATH"] = "/root/darknet_v2.0/:" + os.environ["PATH"]
+os.environ["PATH"] = "/root/darknet_v2.0:" + os.environ["PATH"]
 
-os.system("darknet detector")
+os.system("darknet detector demo cfg/coco.data cfg/yolo.cfg yolo.weights data/2017-08-16_18-07-18.avi -prefix results/frame")
