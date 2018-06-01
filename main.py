cd9b6225c770ee25ce0a734dca24be8bdad1724f
blitzingeagle <mcmorrischen@gmail.com>
2018-06-01 03:54:18 +0000

    Find video contents

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ os.environ["PATH"] = darknet_path + ":" + os.environ["PATH"]
 if len(glob("yolo.weights")) == 0:
     os.system("wget https://pjreddie.com/media/files/yolo.weights")
 
-os.system("darknet detector demo cfg/coco.data cfg/yolo.cfg yolo.weights input/data/2017-08-16_18-07-18.avi -prefix output/frame")
+input_dirs = glob("input/*")
+for input_dir in input_dirs:
+    videos = glob(os.path.join(input_dir, "*"))
+    print(videos)
+
+# os.system("darknet detector demo cfg/coco.data cfg/yolo.cfg yolo.weights input/data/2017-08-16_18-07-18.avi -prefix output/frame")
