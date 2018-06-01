6a1971599774bcae3aceffdb0cef648f28c1208c
blitzingeagle <mcmorrischen@gmail.com>
2018-06-01 03:46:28 +0000

    Migrated Dependencies

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import os
-os.environ["PATH"] = "/root/darknet_v2.0:" + os.environ["PATH"]
+from glob import glob
 
-os.system("darknet detector demo cfg/coco.data cfg/yolo.cfg yolo.weights data/2017-08-16_18-07-18.avi -prefix results/frame")
+darknet_path = "/root/darknet_v2.0"
+os.environ["PATH"] = darknet_path + ":" + os.environ["PATH"]
+
+if len(glob("yolo.weights")) == 0:
+    os.system("wget https://pjreddie.com/media/files/yolo.weights")
+
+os.system("darknet detector demo cfg/coco.data cfg/yolo.cfg yolo.weights input/data/2017-08-16_18-07-18.avi -prefix output/frame")
