878cb004f56732c9efc75cb921a44f0fa12713a7
blitzingeagle <mcmorrischen@gmail.com>
2018-06-04 09:22:34 +0000

    Crop by video frame

--- frame_cnt_main.py
+++ frame_cnt_main.py
@@ -1,6 +1,77 @@
+import os
+from glob import glob
+import json
+from PIL import Image
 import cv2
 import numpy as np
+import re
 
-cv2.imshow("LOL", np.random.random((50,50)))
-cv2.waitKey()
-cv2.destroyAllWindows()
+darknet_path = "/root/darknet_v2.0"
+os.environ["PATH"] = darknet_path + ":" + os.environ["PATH"]
+
+if len(glob("yolo.weights")) == 0:
+    os.system("wget https://pjreddie.com/media/files/yolo.weights")
+
+
+def process_video(video, output_dir):
+    cmd = "darknet detector demo cfg/coco.data cfg/yolo.cfg yolo.weights {} -avg 1 -prefix {}".format(video, os.path.join(output_dir, "frame"))
+    print(">", cmd)
+    os.system(cmd)
+
+
+def crop_image(frame, tag):
+    (top, bot, left, right) = (tag["top"], tag["bot"], tag["left"], tag["right"])
+    print("Top:{} Bot:{} Left:{} Right:{}".format(top, bot, left, right))
+
+    img = frame[...,::-1]
+    return Image.fromarray(img[top:bot, left:right])
+
+
+def target_search(frames, output_dir, target="car"):
+    target_dir = os.path.join(output_dir, target)
+    if not os.path.exists(target_dir):
+        os.makedirs(target_dir)
+
+    with open(os.path.join(output_dir, "frame.txt")) as f:
+        frame_data = [line.strip() for line in f.readlines()]
+
+    for data in frame_data:
+        info = json.loads(data)
+        framename = os.path.basename(info["filename"])
+        tag_list = info["tag"]
+        cnt = 0
+        for tag in tag_list:
+            if target in tag:
+                framenum = int(re.sub("[^0-9]", "", framename))
+                print("Frame:", framenum)
+
+                if 0 <= framenum and framenum < len(frames):
+                    img = crop_image(frames[framenum], tag)
+
+                    file = os.path.join(target_dir, framename.replace(".jpg", "_{}_{:02d}.jpg".format(target, cnt)))
+                    img.save(file)
+                    print(file)
+                    cnt += 1
+
+
+input_dirs = glob("input/*")
+for input_dir in input_dirs:
+    videos = glob(os.path.join(input_dir, "*.avi"))
+
+    for video in videos:
+        output_dir = os.path.splitext(video)[0].replace("input", "output")
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        process_video(video, output_dir)
+
+        cap = cv2.VideoCapture(video)
+        frames = [None]
+        suc, frame = cap.read()
+        while suc:
+            frames.append(frame)
+            suc, frame = cap.read()
+        cap.release()
+        target_search(frames, output_dir)
+
+# os.system("darknet detector demo cfg/coco.data cfg/yolo.cfg yolo.weights input/data/2017-08-16_18-07-18.avi -prefix output/frame")
