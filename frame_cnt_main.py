4767a9f40457506e91551fda241030e7313cc27a
blitzingeagle <mcmorrischen@gmail.com>
2018-06-06 01:57:49 +0000

    Cropper Frame

--- frame_cnt_main.py
+++ frame_cnt_main.py
@@ -6,25 +6,15 @@ import cv2
 import numpy as np
 import re
 
-darknet_path = "/root/darknet_v2.0"
-os.environ["PATH"] = darknet_path + ":" + os.environ["PATH"]
-
-if len(glob("yolo.weights")) == 0:
-    os.system("wget https://pjreddie.com/media/files/yolo.weights")
-
-
-def process_video(video, output_dir):
-    cmd = "darknet detector demo cfg/coco.data cfg/yolo.cfg yolo.weights {} -avg 1 -prefix {}".format(video, os.path.join(output_dir, "frame"))
-    print(">", cmd)
-    os.system(cmd)
+from process_videos import process_videos
 
 
 def crop_image(frame, tag):
     (top, bot, left, right) = (tag["top"], tag["bot"], tag["left"], tag["right"])
     print("Top:{} Bot:{} Left:{} Right:{}".format(top, bot, left, right))
 
-    (height, width) = frame.shape[0:1]
-    pad = 0.2
+    (height, width) = frame.shape[0:2]
+    pad = 0.0
 
     if top < pad * height or bot > (1-pad) * height or left < pad * width or right > (1-pad) * width:
         return None
@@ -38,7 +28,12 @@ def target_search(frames, output_dir, target="car"):
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    with open(os.path.join(output_dir, "frame.txt")) as f:
+    data_file = os.path.join(output_dir.replace("output", "frames"), "frame.txt")
+    if not os.path.isfile(data_file):
+        print("File not found", data_file)
+        return
+
+    with open(data_file) as f:
         frame_data = [line.strip() for line in f.readlines()]
 
     for data in frame_data:
@@ -61,16 +56,16 @@ def target_search(frames, output_dir, target="car"):
                         cnt += 1
 
 
-input_dirs = glob("input/*")
+process_videos()
+
+input_dirs = sorted(glob("input/*"))
 for input_dir in input_dirs:
-    videos = glob(os.path.join(input_dir, "*.avi"))
+    videos = sorted(glob(os.path.join(input_dir, "*.avi")))
 
     for video in videos:
         output_dir = os.path.splitext(video)[0].replace("input", "output")
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
-
-        process_video(video, output_dir)
 
         cap = cv2.VideoCapture(video)
         frames = [None]
